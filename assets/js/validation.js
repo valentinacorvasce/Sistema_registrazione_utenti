750b5e795ea62e093408594e0932303bd1254114
Valentina <lilithsixx@gmail.com>
2021-10-18 21:32:22 +0200

    Text Input - Form Validation

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -6,9 +6,17 @@ const validRegistration = () => {
 
     if (name != '') {
         let characters = name.length;
+        let regEx = /^[a-zA-Z0-9]*[^%&]$/;
 
         if (characters > 12) {
             document.querySelector('label[for="name"]').innerHTML += '<br><div class="alert alert-info">Massimo 12 caratteri!</div>';
+
+            return false;
+
+        }
+
+        if (!regEx.test(name)) {
+            document.querySelector('label[for="name"]').innerHTML += '<br><div class="alert alert-warning">I caratteri speciali non sono permessi!</div>';
 
             return false;
 
