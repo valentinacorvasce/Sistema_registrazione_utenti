d2a146353e375d6b4fe74ca52e8a068f0022caa3
Valentina <lilithsixx@gmail.com>
2021-10-19 20:21:27 +0200

    Email & Password - Form Validation

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -4,6 +4,7 @@ const validRegistration = () => {
     let name = document.querySelector('#name').value;
     // alert(name);
     let mail = document.querySelector('#email').value;
+    let pass = document.querySelector('#pass').value;
 
     // Validazione del Nome;
     if (name != '') {
@@ -27,8 +28,25 @@ const validRegistration = () => {
     }
 
     // Validazione della Email;
-    if () {
+    if (mail != '') {
+        let regEx = /^\S+@\S+\.\S+$/;
 
+        if (!regEx.test(mail)) {
+            document.querySelector('label[for="email"]').innerHTML += '<br><div class="alert alert-warning">Ops, l\'indirizzo mail inserito non è valido</div>';
+
+            return false;
+        }
+    }
+
+    // Validazione della Password;
+    if (pass != '') {
+        let regEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+        if (!regEx.test(pass)) {
+            document.querySelector('label[for="pass"]').innerHTML += '<br><div class="alert alert-warning">SONO NECESSARI:<br><br> almeno 8 caratteri,<br> una lettera maiuscola,<br> un numero,<br> un carattere speciale</div>';
+
+            return false;
+        }
     }
 
 
--- views/modules/register.php
+++ views/modules/register.php
@@ -23,8 +23,8 @@
     <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="La tua Mail" name="mail" required>
   </div>
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="La tua Password" name="password" required>
+    <label for="pass" class="form-label">Password</label>
+    <input type="password" class="form-control" id="pass" placeholder="La tua Password" name="password" required>
   </div>
   <div class="mb-3 form-check">
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
