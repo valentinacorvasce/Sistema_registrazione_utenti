c7cbd7c878fa1a3a003363b88ed42f2527165696
valentinacorvasce <71196417+valentinacorvasce@users.noreply.github.com>
2021-12-03 20:52:51 +0100

    Update logout.php

--- views/modules/logout.php
+++ views/modules/logout.php
@@ -3,6 +3,10 @@
 session_start();
 session_destroy();
 
+if(!isset($_SESSION['validation'])){     
+	header("Location:../../index.php"); 
+   }
+
 ?>
 
 <h1>Hai eseguito correttamente il Logout!</h1>
